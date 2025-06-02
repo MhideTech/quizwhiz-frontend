78c98ca9572ce0b344b3a34480bd4024b1f3a54f
divineamunega <divineamunega@gmail.com>
2025-06-02 16:36:15 +0100

    env chanegs

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,7 +1,12 @@
+// This is jpw to import env variables in Vite
+const API_URL = import.meta.env.VITE_API_URL;
+console.log(API_URL);
+
+// TODO change this to async await
 function signUpWithEmail(data) {
-  return fetch("https://example.com/api", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  }).then((res) => res.json());
+	return fetch("https://example.com/api", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(data),
+	}).then((res) => res.json());
 }
