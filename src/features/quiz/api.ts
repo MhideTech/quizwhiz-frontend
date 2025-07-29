13ed411b30db298f8d4ca890e6108a9135fa7cf4
divineamunega <divineamunega@gmail.com>
2025-07-30 00:30:26 +0100

    some changes

--- src/features/quiz/api.ts
+++ src/features/quiz/api.ts
@@ -1,7 +1,8 @@
 import axiosInstance from '@/common/api/axiosInstance';
 import { CreateQuizFormType } from './types';
+const quizRoute = 'quiz';
 
 export async function createQuiz(quizData: CreateQuizFormType) {
-  const { data } = await axiosInstance.post('quiz', quizData);
+  const { data } = await axiosInstance.post(quizRoute, quizData);
   return data;
 }
