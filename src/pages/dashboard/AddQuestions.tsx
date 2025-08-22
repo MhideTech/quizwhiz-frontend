4f3a6a359c6d32df8e5f80a6a1203be9cac96ad2
divineamunega <divineamunega@gmail.com>
2025-08-22 17:39:28 +0100

    feat: Add AddQuestionsHeader component and update AddQuestions page

--- src/pages/dashboard/AddQuestions.tsx
+++ src/pages/dashboard/AddQuestions.tsx
@@ -1,25 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { useParams, useNavigate, useLocation } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import { Button } from '@/common/components/ui/button';
-import { Input } from '@/common/components/ui/input';
-import { Label } from '@/common/components/ui/label';
-import { Card, CardContent, CardHeader, CardTitle } from '@/common/components/ui/card';
-import { Badge } from '@/common/components/ui/badge';
-import { useToast } from '@/hooks/use-toast';
-import { Trash2, Plus, ArrowLeft, CheckCircle } from 'lucide-react';
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { addQuestionToQuiz, getQuestions, getQuiz } from '@/features/quiz/manage/api.ts';
-
-import { QuestionFormData, Quiz as QuizType } from '@/features/quiz/manage/types';
 import AddQuestionsForm from '@/features/quiz/manage/components/AddQuestionsForm';
 import PreviouslyAddedQuestions from '@/features/quiz/manage/components/PreviouslyAddedQuestions';
 import { useQuestions, useQuiz, useShouldFetch } from '@/features/quiz/manage/hooks';
+import AddQuestionsHeader from '@/features/quiz/manage/components/AddQuestionsHeader';
 
 const AddQuestion = () => {
-  const navigate = useNavigate();
-
-  // todo better name for this
+  // todo find a better name for this
   const { id, shouldFetch, locationState } = useShouldFetch();
   const {
     quizData,
@@ -38,29 +23,10 @@ const AddQuestion = () => {
 
   return (
     <div className='container mx-auto p-6 max-w-4xl'>
-      {/* Header */}
-      <div className='mb-6'>
-        <Button variant='ghost' onClick={() => navigate('/dashboard')} className='mb-4'>
-          <ArrowLeft className='w-4 h-4 mr-2' />
-          Back to Dashboard
-        </Button>
-        <div className='flex items-center gap-3 mb-2'>
-          <h1 className='text-3xl font-bold'>Add Questions</h1>
-          {/* <Badge variant='secondary'>{questions.length} questions</Badge> */}
-        </div>
-        <p className='text-muted-foreground'>
-          Adding questions to:{' '}
-          {fetchPending ? (
-            <span className='inline-block h-4 w-32 bg-gray-300 rounded animate-pulse'></span>
-          ) : (
-            <span className='font-medium'>{quizData.title}</span>
-          )}
-        </p>
-      </div>
+      <AddQuestionsHeader quizTitle={quizData?.title} isLoading={fetchPending} />
 
       <div className='grid grid-cols-1 lg:grid-cols-3 gap-6'>
         <AddQuestionsForm />
-
         <PreviouslyAddedQuestions
           isPending={questionPending}
           isSuccess={questionSuccess}
